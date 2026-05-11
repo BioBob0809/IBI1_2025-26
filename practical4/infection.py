569f4e6acfc1c807fd5bf4886734273112185ce6
BioBob0809 <jinfeng.25@intl.zju.edu.cn>
2026-05-11 11:42:09 +0100

    Week4: Fixed infection.py to correctly display daily increasing infected counts (starts with 5, 40% growth rate)

--- practical4/infection.py
+++ practical4/infection.py
@@ -1,45 +1,41 @@
-# --------------------------
-# PSEUDOCODE
-# --------------------------
+# Pseudocode
 # 1. Define initial parameters:
 #    - total_students = 91 (the whole class size)
-#    - a = the number of initiative infected students that is inputed 
+#    - initial_infected = 5 (number of initially infected students)
 #    - growth_rate = 0.4 (40% daily growth rate)
 #    - day = 1 (start counting from day 1)
 # 2. While the number of infected students is less than total_students:
 #    a. Print the current number of infected students for this day
 #    b. Calculate the new number of infected students: new_infected = infected * (1 + growth_rate)
-#    c. Round the new number to the nearest whole number (since we can't have partial people)
-#    d. If the new number exceeds total_students, set it to total_students (to cap at full class)
+#    c. Round the new number to the nearest whole number
+#    d. If the new number exceeds total_students, set it to total_students
 #    e. Increment the day counter
 # 3. After the loop ends, print the total number of days taken to infect all students
-# --------------------------
 
-a = int(input("The number of initiative infected students is："))
+# Parameters
+total_students = 91
+infected = 5
 growth_rate = 0.4
 day = 1
-while a <= 91:
-    pri_a = a
-    a = a*(1+growth_rate)
-    a = round (a)
-    if a > 91:
-        print ("Number of infected students：", 91-pri_a,)
-    else:
-        print ("Number of infected students：",a-a/1.4, )
-    day +=1
-print (f"It took {day} days for all 91 students to become infected.7")
 
+print("Day | Number of infected students")
+print("-------------------------------")
 
-# Result:
+# Loop while not all students are infected
+while infected < total_students:
+    # Print current day's infected count
+    print(f"{day:3} | {infected}")
+    
+    # Calculate next day's infected count
+    infected = round(infected * (1 + growth_rate))
+    
+    # Cap at total students if needed
+    if infected > total_students:
+        infected = total_students
+    
+    day += 1
 
-# The number of initiative infected students is：5
-# Number of infected students： 2.0
-# Number of infected students： 2.8571428571428568
-# Number of infected students： 4.0
-# Number of infected students： 5.7142857142857135
-# Number of infected students： 8.0
-# Number of infected students： 11.142857142857142
-# Number of infected students： 15.714285714285715
-# Number of infected students： 22.0
-# Number of infected students： 14
-# It took 10 days for all 91 students to become infected.
+# Print the final day when all are infected
+print(f"{day:3} | {infected}")
+
+print(f"\nIt took {day} days for all {total_students} students to become infected.")
